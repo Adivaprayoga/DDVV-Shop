c274fdfdf3b64689f370fceb83c1aefb625caef0
dedevava <adiva008@gmail.com>
2019-05-06 15:38:06 +0800

    banner final

--- js/app.js
+++ js/app.js
@@ -6,11 +6,22 @@ window.addEventListener('scroll', function() {
 function setNavbarBg() {
 	var top  = window.pageYOffset || document.documentElement.scrollTop;
 	var navbar = document.getElementById('header');
+	var logo = document.getElementById('logo-text');
+	var login = document.getElementById('nav-login');
+	var	signup = document.getElementById('nav-signup');
 
 	if (top > 30) {
 		navbar.classList.add('header-scrolled');
+		logo.style.color = "#FF5A5F";
+		logo.style.letterSpacing = "10px";
+		signup.style.color = "#484848";
+		login.style.color = "#484848";
 	}else{
 		navbar.classList.remove('header-scrolled');
+		logo.style.color = "#FFF";
+		logo.style.letterSpacing = "-5px";
+		signup.style.color = "#FFF";
+		login.style.color = "#FFF";
 	}
 }
 
